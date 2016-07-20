f3760a9e24661f0f9058b8414b6988eec78e6d3b
Andy <smrtwater@126.com>
2016-07-20 22:04:51 +0800

    同步异步函数却别

--- 07-多线程-GCD基本使用/07-多线程-GCD基本使用/ViewController.m
+++ 07-多线程-GCD基本使用/07-多线程-GCD基本使用/ViewController.m
@@ -21,9 +21,109 @@
 
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-//    创建并发队列
+    [self syncMain];
+}
+
+/**
+ *  同步函数 + 主线程 会阻塞
+ */
+-(void)syncMain
+{
+    // 获得主队列
+    dispatch_queue_t queue = dispatch_get_main_queue();
+    
+    dispatch_sync(queue, ^{
+        NSLog(@"1--%@",[NSThread currentThread]);
+    });
+    dispatch_sync(queue, ^{
+        NSLog(@"2--%@",[NSThread currentThread]);
+    });
+    dispatch_sync(queue, ^{
+        NSLog(@"3--%@",[NSThread currentThread]);
+    });
+}
+
+/**
+ *  异步函数 + 主队列 不会开启线程，只会在主线程中执行
+ */
+-(void)asyncMain
+{
+    // 获得主队列
+    dispatch_queue_t queue = dispatch_get_main_queue();
+    
+    dispatch_async(queue, ^{
+        NSLog(@"1--%@",[NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"2--%@",[NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"3--%@",[NSThread currentThread]);
+    });
+}
+
+/**
+ *  同步函数 + 串行队列，不会开启线程
+ */
+-(void)syncSerial
+{
+    dispatch_queue_t queue = dispatch_queue_create("serialQueue", DISPATCH_QUEUE_SERIAL);// 串行队列
+    dispatch_sync(queue, ^{
+        NSLog(@"1--%@",[NSThread currentThread]);
+    });
+    dispatch_sync(queue, ^{
+        NSLog(@"2--%@",[NSThread currentThread]);
+    });
+    dispatch_sync(queue, ^{
+        NSLog(@"3--%@",[NSThread currentThread]);
+    });
+}
+
+
+/**
+ *  异步函数 + 串行队列，会开启新的线程，任务串行
+ */
+-(void)asyncSerial
+{
+    dispatch_queue_t queue = dispatch_queue_create("serialQueue", DISPATCH_QUEUE_SERIAL);// 串行队列
+    dispatch_async(queue, ^{
+        NSLog(@"1--%@",[NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"2--%@",[NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"3--%@",[NSThread currentThread]);
+    });
+}
+
+/**
+ *  同步函数--并发队列，不会开启新的线程，
+ */
+-(void)syncConcurrent
+{
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);// 并发队列
+    dispatch_sync(queue, ^{
+        NSLog(@"%@",[NSThread currentThread]);
+    });
+    dispatch_sync(queue, ^{
+        NSLog(@"%@",[NSThread currentThread]);
+    });
+    dispatch_sync(queue, ^{
+        NSLog(@"%@",[NSThread currentThread]);
+    });
+}
+
+/**
+ *  异步函数--并发队列，可以同时开启多条线程
+ */
+-(void)asyncConcurrent
+{
+    //    创建并发队列
     //   1，label:队列的名字
-    dispatch_queue_t queue = dispatch_queue_create("download.image.queue", DISPATCH_QUEUE_CONCURRENT);
+//    dispatch_queue_t queue = dispatch_queue_create("download.image.queue", DISPATCH_QUEUE_CONCURRENT);
+    // 2，获得全局的并发队列
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     dispatch_async(queue, ^{
         NSLog(@"%@",[NSThread currentThread]);
     });
@@ -34,7 +134,6 @@
         NSLog(@"%@",[NSThread currentThread]);
     });
 
-    
 }
 
 @end
