a982a4d40b91b7f149adcd5f6dc5a192c3894e18
Andy <smrtwater@126.com>
2016-08-14 23:10:40 +0800

    网络-NSURLConnection修改

--- 01-网络-NSURLConnection/01-网络-NSURLConnection/ViewController.m
+++ 01-网络-NSURLConnection/01-网络-NSURLConnection/ViewController.m
@@ -9,10 +9,12 @@
 #import "ViewController.h"
 #import "SVProgressHUD.h"
 
-@interface ViewController ()
+@interface ViewController ()<NSURLConnectionDataDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *usernameField;
 @property (weak, nonatomic) IBOutlet UITextField *pwdField;
 
+/** <#注释#> */
+@property(nonatomic,strong)NSMutableData* responsedata;
 @end
 
 @implementation ViewController
@@ -63,7 +65,21 @@
 
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
+    [self delegateAsync];
+}
 
+-(void)delegateAsync
+{
+    // 1,路径
+    NSURL* url = [NSURL URLWithString:@"http://120.25.226.186:32812/login?username=520it&pwd=520it"];
+    // 2，请求对象
+    NSURLRequest* request = [NSURLRequest requestWithURL:url];
+    // 3,创建连接对象
+    NSURLConnection* conn = [[NSURLConnection alloc]initWithRequest:request delegate:self];
+    /**
+     *  取消请求
+     */
+//    [conn cancel];
 }
 
 -(void)AsyncGet
@@ -80,7 +96,41 @@
         NSLog(@"%@",str);
     }];
 }
+#pragma mark --<NSURLConnectionDataDelegate>--begin
 
+-(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+   NSLog(@"didFailWithError----%@",error);
+}
+/**
+ *  接收到服务器的相应
+ */
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    self.responsedata = [NSMutableData data];
+    NSLog(@"didReceiveResponse----%@",response);
+}
+/**
+ *  接收数据，如果数据量比较大，这个函数会调用多次
+ */
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+//    大文件不能用这种方法
+    [self.responsedata appendData:data];
+    NSLog(@"didReceiveData----%zd",data.length);
+}
+/**
+ *  数据接收完毕
+ */
+-(void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+     NSLog(@"DidFinishLoading----%@",connection);
+    NSString* str = [[NSString alloc]initWithData:self.responsedata encoding:NSUTF8StringEncoding];
+    NSLog(@"%@",str);
+    self.responsedata = nil;
+}
+
+#pragma mark --<NSURLConnectionDataDelegate>---end
 -(void)get
 {
     NSURL* url = [NSURL URLWithString:@"http://120.25.226.186:32812/login?username=520it&pwd=1"];
