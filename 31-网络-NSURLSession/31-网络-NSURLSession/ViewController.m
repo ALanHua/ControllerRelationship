dc0b03468ce545b9d80e9bec32137718a2461a9c
Andy <smrtwater@126.com>
2016-08-24 20:53:45 +0800

    NSURLSession下载文件

--- 31-网络-NSURLSession/31-网络-NSURLSession/ViewController.m
+++ 31-网络-NSURLSession/31-网络-NSURLSession/ViewController.m
@@ -20,10 +20,29 @@
     
 }
 
-
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    [self post];
+    [self download];
+}
+
+-(void)download
+{
+    // 获得NSURLSession对象
+    NSURLSession* session = [NSURLSession sharedSession];
+    // 获得下载任务
+    NSURL* url = [NSURL URLWithString:@"http://120.25.226.186:32812/resources/videos/minion_15.mp4"];
+    NSURLSessionDownloadTask* task = [session downloadTaskWithURL:url completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+//        NSLog(@"文件下载完毕---%@",location);
+        // 文件将来存放的真实路径
+        NSString* caches = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)lastObject];
+        NSString* file = [caches stringByAppendingPathComponent:response.suggestedFilename];
+        // 剪切location的临时文件到真实路径
+        NSFileManager* mgr = [NSFileManager defaultManager];
+        [mgr moveItemAtURL:location toURL:[NSURL fileURLWithPath:file]  error:nil];
+        NSLog(@"%@",file);
+    }];
+    // 启动任务
+    [task resume];
 }
 
 -(void)post
