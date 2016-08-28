2f647a5c3d1ad2ecaa0426329bc8126495a8baba
Andy <smrtwater@126.com>
2016-08-28 22:58:27 +0800

    AFNetWorking监听网络状态

--- 35-网络-AFNetworking/35-网络-AFNetworking/ViewController.m
+++ 35-网络-AFNetworking/35-网络-AFNetworking/ViewController.m
@@ -7,11 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "Reachability.h"
 #import <AFNetworking.h>
 
-@interface ViewController ()
+@interface ViewController ()<NSXMLParserDelegate>
 @property (weak, nonatomic) IBOutlet UIProgressView *progressView;
-
+/** 监控网路 */
+@property(nonatomic,strong)Reachability* reachability;
 @end
 
 @implementation ViewController
@@ -19,12 +21,96 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
    
+    [self appleMonitoring];
+}
+
+-(void)appleMonitoring
+{
+    // 监听通知
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(getNetWorkStatus:) name:kReachabilityChangedNotification object:nil];
     
+    // 监控网络
+    self.reachability = [Reachability reachabilityForInternetConnection];
+    [self.reachability startNotifier];
+      
+}
+-(void)dealloc
+{
+    [[NSNotificationCenter defaultCenter]removeObserver:self];
+    [self.reachability stopNotifier];
+    self.reachability = nil;
+}
+
+-(void)getNetWorkStatus:(NSNotification*)note
+{
+    NSLog(@"%@",note);
+    
+    if([Reachability reachabilityForLocalWiFi].currentReachabilityStatus !=
+       NotReachable){
+        NSLog(@"status is wifi network");
+    }else if([Reachability reachabilityForInternetConnection].currentReachabilityStatus != NotReachable){
+        NSLog(@"status is Phone local network");
+    }else{
+        NSLog(@"UnKnow NetWork");
+    }
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    [self upload];
+//    [self upload];
+    AFHTTPSessionManager* mgr = [AFHTTPSessionManager manager];
+    /**
+     *  [AFJSONResponseSerializer serializer];         //JSON解析器
+     *  [AFXMLParserResponseSerializer serializer];    //XML解析器
+     *  [AFHTTPResponseSerializer serializer];         //不做任何解析
+     */
+
+    mgr.responseSerializer = [AFHTTPResponseSerializer serializer];
+    
+    NSString* urlString = @"http://120.25.226.186:32812/resources/images/minion_15.png";
+    
+    [mgr GET:urlString parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, NSData* responseObject) {
+        NSLog(@"---succcess:%ld",responseObject.length);
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"---failure--");
+    }];
+}
+
+-(void)AFCheckNetWorkMemthod
+{
+    // 开始网络监控
+    AFNetworkReachabilityManager* mgr = [AFNetworkReachabilityManager sharedManager];
+    [mgr setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+        NSLog(@"----now status:%zd",status);
+    }];
+    
+    [mgr startMonitoring];
+    // 拿到当前网络状态
+    [mgr networkReachabilityStatus];
+}
+
+-(void)xmlGet
+{
+    AFHTTPSessionManager* mgr = [AFHTTPSessionManager manager];
+    // 已XML形式解析
+    mgr.responseSerializer  = [AFXMLParserResponseSerializer serializer];
+    
+    NSString* urlString = @"http://120.25.226.186:32812/login";
+    NSDictionary* params =  @{
+                              @"username":@"520it",
+                              @"pwd" : @"520it",
+                              @"type" : @"XML"
+                              };
+    
+    [mgr GET:urlString parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, NSXMLParser* parser) {
+        //        NSLog(@"---succcess:%@",responseObject);
+        parser.delegate = self;
+        [parser parse];
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"---failure--");
+    }];
+    
 }
 
 /**
