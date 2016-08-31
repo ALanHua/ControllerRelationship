a7dd34d40f473d0b16cdd3df404cd0c38ce861ae
Andy <smrtwater@126.com>
2016-08-31 22:09:11 +0800

    JS调用OC方法

--- 39-网络-网页相关/39-网络-网页相关/ViewController.m
+++ 39-网络-网页相关/39-网络-网页相关/ViewController.m
@@ -32,19 +32,53 @@
     [self.webView loadRequest:[NSURLRequest requestWithURL: [[NSBundle mainBundle]URLForResource:@"index" withExtension:@"html"]]];
 }
 
--(void)call
+-(void)call:(NSString*)number
 {
-    UIApplication* app = [UIApplication sharedApplication];
-    [app openURL:[NSURL URLWithString:@"tell://10010"]];
+//    UIApplication* app = [UIApplication sharedApplication];
+//    [app openURL:[NSURL URLWithString:@"tell://10010"]];
+    NSLog(@"%s,%@",__func__,number);
+}
+
+-(void)sendMessage:(NSString*)number
+{
+    NSLog(@"%s,%@",__func__,number);
+}
+
+-(void)openCamera:(NSString*)number
+{
+    NSLog(@"%s,%@",__func__,number);
 }
 
 #pragma mark - <UIWebViewDelegate>
 -(void)webViewDidFinishLoad:(UIWebView *)webView
 {
-    NSString* result = [webView stringByEvaluatingJavaScriptFromString:@"login();"];
-    NSLog(@"%@",result);
+//    NSString* result = [webView stringByEvaluatingJavaScriptFromString:@"login();"];
+//    NSLog(@"%@",result);
 //   self.title = [webView stringByEvaluatingJavaScriptFromString:@"document.title"];
     
 
 }
+
+/*JS 调用OC代码
+ * JS 第三方框架:webViewJavaScriptBridge
+ */
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    
+//    NSLog(@"%@",request.URL);
+    NSString* url = request.URL.absoluteString;
+    NSString* scheme = @"yhp://";
+    if ([url hasPrefix:scheme]) {
+        NSString* path = [url substringFromIndex:scheme.length];
+        NSArray* subPaths = [path componentsSeparatedByString:@"?"];
+        // 方法名
+        NSString* methodName = [[subPaths firstObject] stringByReplacingOccurrencesOfString:@"_" withString:@":"];
+        NSString* params = [subPaths lastObject];
+        [self performSelector:NSSelectorFromString(methodName) withObject:params];
+        return  NO;
+    }
+    
+    return YES;
+}
+
 @end
