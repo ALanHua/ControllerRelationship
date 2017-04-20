b58f47c9bf12325957f45860fa1490cb835b1c4a
Andy <smrtwater@126.com>
2017-04-20 23:22:48 +0800

    内购代码初始化

--- 40-内购/40-内购/ViewController.m
+++ 40-内购/40-内购/ViewController.m
@@ -10,7 +10,7 @@
 #import <StoreKit/StoreKit.h>
 
 
-@interface ViewController ()<SKProductsRequestDelegate>
+@interface ViewController ()<SKProductsRequestDelegate,SKPaymentTransactionObserver>
 /** 所有商品 */
 @property(nonatomic,strong)NSArray* products;
 @end
@@ -39,7 +39,8 @@
     self.products = [response.products sortedArrayWithOptions:NSSortConcurrent usingComparator:^NSComparisonResult(SKProduct* obj1, SKProduct* obj2) {
         return [obj1.price compare:obj2.price];
     }];
-    
+    // 刷新表格
+    [self.tableView reloadData];
 }
 
 #pragma mark - <UITableViewDataSource>
@@ -56,10 +57,75 @@
         cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
     }
     // 取出模型
-    
+    SKProduct* product = self.products[indexPath.row];
+    cell.textLabel.text = product.localizedTitle;
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"价格：%@",product.price];
     
     return cell;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // 取出模型
+    SKProduct* product = self.products[indexPath.row];
+    // 购买商品
+    [self buyProduct:product];
+}
+
+#pragma mark - 购买商品
+-(void)buyProduct:(SKProduct*)product
+{
+    // 1,创建票据
+    SKPayment* payment = [SKPayment paymentWithProduct:product];
+    // 2，将票据加入到队列中
+    [[SKPaymentQueue defaultQueue]addPayment:payment];
+    // 添加观察者
+    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+}
+
+#pragma mark - <SKPaymentTransactionObserver>
+
+/**
+ 交易发生改变就会调用该方法
+ @param queue 队列
+ @param transactions 交易
+ */
+- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray<SKPaymentTransaction *> *)transactions
+{
+    /**
+     SKPaymentTransactionStatePurchasing, 正在购买
+     SKPaymentTransactionStatePurchased,  已经购买
+     SKPaymentTransactionStateFailed,     购买失败
+     SKPaymentTransactionStateRestored,   恢复购买
+     SKPaymentTransactionStateDeferred    小孩购买
+     */
+    for (SKPaymentTransaction* transaction in transactions) {
+        switch (transaction.transactionState) {
+            case SKPaymentTransactionStatePurchasing:
+                NSLog(@"正在购买");
+                break;
+            case SKPaymentTransactionStatePurchased:
+                NSLog(@"购买成功，商品给用户");
+                // 将交易从队列从移除
+                [queue finishTransaction:transaction];
+                break;
+            case SKPaymentTransactionStateFailed:
+                 NSLog(@"购买失败");
+                // 将交易从队列从移除
+                [queue finishTransaction:transaction];
+                break;
+            case SKPaymentTransactionStateRestored:
+                NSLog(@"恢复购买,对应商品给用户");
+                // 将交易从队列从移除
+                [queue finishTransaction:transaction];
+                break;
+            case SKPaymentTransactionStateDeferred:
+                NSLog(@"未决定");
+                break;
+            default:
+                break;
+        }
+    }
+}
 
 @end
