90e7926d91935c2b9d84710f2f0741ec5e15b9f1
Andy <smrtwater@126.com>
2017-05-10 22:38:17 +0800

    1,创建远程通知工程

---
+++ 47-远程通知/47-远程通知/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  47-远程通知
+//
+//  Created by yhp on 2017/5/10.
+//  Copyright © 2017年 YouHuaPei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 47-远程通知/47-远程通知/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  47-远程通知
+//
+//  Created by yhp on 2017/5/10.
+//  Copyright © 2017年 YouHuaPei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
