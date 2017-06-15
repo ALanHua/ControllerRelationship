5824ada22801c98231b2198a2e223ed800c87e7b
Andy <smrtwater@126.com>
2017-06-15 23:37:44 +0800

    简单生成二维码

---
+++ 56-二维码/56-二维码/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  56-二维码
+//
+//  Created by yhp on 2017/6/15.
+//  Copyright © 2017年 YouHuaPei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 56-二维码/56-二维码/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  56-二维码
+//
+//  Created by yhp on 2017/6/15.
+//  Copyright © 2017年 YouHuaPei. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <CoreImage/CoreImage.h>
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+}
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    // 创建过滤器
+    CIFilter* filter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
+    // 恢复默认设置
+    [filter setDefaults];
+    // 给过滤器添加数据
+    NSString* dataString = @"youhuapei smart";
+    NSData* data = [dataString dataUsingEncoding:NSUTF8StringEncoding];
+    [filter setValue:data forKeyPath:@"inputMessage"];
+    // 获取输出二维码
+    CIImage* outputImage = [filter outputImage];
+    self.imageView.image = [UIImage imageWithCIImage:outputImage];
+}
+
+@end
