acc8b33a762889bfd4a9bdcc01440ad13f9522ce
Andy <smrtwater@126.com>
2017-07-05 23:31:39 +0800

    push 及 pull 方法获取陀螺仪及加速度信息

--- 66-coreMotion/66-coreMotion/ViewController.m
+++ 66-coreMotion/66-coreMotion/ViewController.m
@@ -19,14 +19,60 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-
+    // pull
+    if (![self.mgr isGyroAvailable]) {
+        NSLog(@"手机该换了");
+    }
+    
+    [self.mgr startGyroUpdates];
     
 }
+
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    CMRotationRate rotationrate = self.mgr.gyroData.rotationRate;
+    NSLog(@"x:%f,y:%f,z:%f",rotationrate.x,rotationrate.y,rotationrate.z);
+}
+
+#pragma mark - 获取陀螺仪信息
+- (void)pushGyro
+{
+    // 获取陀螺仪
+    if (![self.mgr isGyroAvailable]) {
+        NSLog(@"手机该换了");
+    }
+    
+    // 设置采样间隔
+    self.mgr.gyroUpdateInterval = 0.3;
+    
+    // 开始采样
+    [self.mgr startGyroUpdatesToQueue:[NSOperationQueue mainQueue] withHandler:^(CMGyroData * _Nullable gyroData, NSError * _Nullable error) {
+        if (error) {
+            return;
+        }
+        
+        CMRotationRate rate = gyroData.rotationRate;
+        NSLog(@"x:%f,y:%f,z:%f",rate.x,rate.y,rate.z);
+    }];
+}
+
 #pragma mark - 获取加速计信息
--(void)acceleramiter
+-(void)pullAccelerameter
+{
+    // pull
+    // 判断加速度计是否可用
+    if (!self.mgr.isAccelerometerAvailable) {
+        NSLog(@"加速度计不可用");
+        return;
+    }
+    // 开始采样
+    [self.mgr startAccelerometerUpdates];
+}
+
+-(void)pushAccelerameter
 {
     // push 运动管理者对象
-    self.mgr = [[CMMotionManager alloc]init];
     // 判断加速计是否可用
     if (!self.mgr.isAccelerometerAvailable) {
         NSLog(@"加速计不可用");
@@ -44,5 +90,22 @@
         NSLog(@"x:%f,y:%f,z:%f",acceleration.x,acceleration.y,acceleration.z);
     }];
 }
+
+-(void)getAcceleramiterInfo
+{
+    CMAcceleration acceleration = self.mgr.accelerometerData.acceleration;
+    NSLog(@"x:%f,y:%f,z:%f",acceleration.x,acceleration.y,acceleration.z);
+}
+
+#pragma mark - mgr 懒加载
+- (CMMotionManager *)mgr
+{
+    if (!_mgr) {
+        _mgr = [[CMMotionManager alloc]init];
+    }
+    return _mgr;
+}
+
+
 
 @end
