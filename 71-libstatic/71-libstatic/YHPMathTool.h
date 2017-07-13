191909c69fe76967c0e8d015e7659df56ee8661d
Andy <smrtwater@126.com>
2017-07-13 23:44:56 +0800

    学习静态库的生成

---
+++ 71-libstatic/71-libstatic/YHPMathTool.h
@@ -0,0 +1,15 @@
+//
+//  YHPMathTool.h
+//  71-libstatic
+//
+//  Created by yhp on 2017/7/13.
+//  Copyright © 2017年 YouHuaPei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YHPMathTool : NSObject
+
++(NSInteger)sumNum1:(NSInteger)num1 num2:(NSInteger)num2;
+
+@end
---
+++ 71-libstatic/71-libstatic/YHPMathTool.m
@@ -0,0 +1,18 @@
+//
+//  YHPMathTool.m
+//  71-libstatic
+//
+//  Created by yhp on 2017/7/13.
+//  Copyright © 2017年 YouHuaPei. All rights reserved.
+//
+
+#import "YHPMathTool.h"
+
+@implementation YHPMathTool
+
++(NSInteger)sumNum1:(NSInteger)num1 num2:(NSInteger)num2
+{
+    return num1 + num2;
+}
+
+@end
