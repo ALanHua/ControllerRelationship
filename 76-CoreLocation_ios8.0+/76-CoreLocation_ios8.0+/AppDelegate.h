54442ed36531f9eeb317c0207496c68766b10cfa
Andy <smrtwater@126.com>
2017-07-19 23:01:12 +0800

    监听定位隐私改变状态

---
+++ 76-CoreLocation_ios8.0+/76-CoreLocation_ios8.0+/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  76-CoreLocation_ios8.0+
+//
+//  Created by yhp on 2017/7/19.
+//  Copyright © 2017年 YouHuaPei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 76-CoreLocation_ios8.0+/76-CoreLocation_ios8.0+/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  76-CoreLocation_ios8.0+
+//
+//  Created by yhp on 2017/7/19.
+//  Copyright © 2017年 YouHuaPei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
