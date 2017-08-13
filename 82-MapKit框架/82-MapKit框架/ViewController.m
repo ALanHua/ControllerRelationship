1ac60e9e8bab86c63468d3a20ae01b4d06e67b7a
Andy <smrtwater@126.com>
2017-08-13 21:32:54 +0800

    设置region

--- 82-MapKit框架/82-MapKit框架/ViewController.m
+++ 82-MapKit框架/82-MapKit框架/ViewController.m
@@ -10,7 +10,7 @@
 #import <MapKit/MapKit.h>
 #import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
+@interface ViewController ()<MKMapViewDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 /** 位置管理者 */
 @property(nonatomic,strong)CLLocationManager* lm;
@@ -39,13 +39,38 @@
 
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    self.mapView.mapType = MKMapTypeHybridFlyover;
+    self.mapView.mapType = MKMapTypeStandard;
 //    self.mapView.zoomEnabled = YES;
 //    self.mapView.showsScale = YES;
 //    self.mapView.showsCompass = YES;
     [self lm];
-//    self.mapView.showsUserLocation = YES;
-    self.mapView.userTrackingMode = MKUserTrackingModeFollowWithHeading;
+    self.mapView.showsUserLocation = YES;
+//    self.mapView.userTrackingMode = MKUserTrackingModeFollowWithHeading;
+}
+
+#pragma mark - <MKMapViewDelegate>
+
+/**
+ 更新到位置
+
+ @param mapView 地图
+ @param userLocation 位置对象
+ */
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+    userLocation.title = @"yhp";
+    userLocation.subtitle = @"andy";
+    
+    //  设置地图展示中心
+    [self.mapView setCenterCoordinate:userLocation.location.coordinate animated:YES];
+    MKCoordinateSpan span = MKCoordinateSpanMake(0.055737, 0.001);
+    MKCoordinateRegion reginon = MKCoordinateRegionMake(userLocation.location.coordinate, span);
+    [self.mapView setRegion:reginon animated:YES];
+}
+
+- (void)mapView:(MKMapView *)mapView regionWillChangeAnimated:(BOOL)animated
+{
+//    NSLog(@"%f,%f",mapView.region.span.latitudeDelta,mapView.region.span.longitudeDelta);
 }
 
 
